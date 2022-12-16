c47f1852be2bc4fcbf6827178290b1428e5385a9
envaleed <jmarkanthony.062@gmail.com>
2022-12-16 10:03:29 -0500

    updated product prices and added new urls for products tests

--- test/data/urls.data.js
+++ test/data/urls.data.js
@@ -2,4 +2,7 @@ module.exports = {
   auth_route: 'https://dev-mlluudmotpwoldtv.us.auth0.com/login',
   cart_route: '/cart',
   overview_page_route: '/products',
+  product_route: '/laptop-2',
+  image_url:
+    'https://images.pexels.com/photos/3776438/pexels-photo-3776438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
 };
